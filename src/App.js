7a985e0fa8efb93e345b2fe54b32f2b105681836
Tomasz Wnuk <tomasz.wnuk1@gmail.com>
2022-04-13 19:46:05 +0200

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { StyleSheet } from 'react';
+import ReactDOM from 'react-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App" style={{}}>
+      <div className='mainContainer center'>
+        <div className='header'>
+          <header>
+            <h1 className='title'>Fantastic Names</h1>
+            <p className='info'>This tool will help you create fantasy names. </p>
+            <p className='info'>Just type few syllabes in the left column, choose the length of the word and get the results in the right column.</p>
+          </header>
+        </div>
+        <div className='inputs'>
+          <div className='left-column'>
+            aa
+          </div>
+          <div className='right-column'>
+            bb
+          </div>
+        </div>
+      </div>
+    </div >
   );
 }
 
