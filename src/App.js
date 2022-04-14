d88d76d81f0968b90a37af7fdf3adff5da2890e5
Tomasz Wnuk <tomasz.wnuk1@gmail.com>
2022-04-14 13:59:43 +0200

    added readme

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
 import React, { useState } from 'react';
+import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
 import ReactDOM from 'react-dom';
+
+import _, { map } from 'underscore';
 
 function App() {
 
-  const [numberOfSyllabes, setNumberOfSyllabes] = useState(3);
+  const [numberOfSyllabes, setNumberOfSyllabes] = useState(2);
   const [syllabes, setSyllabes] = useState('');
   const [resultText, setResultText] = useState('');
 
   const fantasticNames = (syllabes) => {
-    let result = ''
-    let textArr = syllabes.split(',');
+    let text = syllabes.replace(/\s/g, '');
+    let textArr = text.split(',');
 
-    let listItems = textArr.map((item) => <li>{item}</li>);
+    //cartesian product
+    const cartesian =
+      (...a) => a.reduce((a, b) => a.flatMap(d => b.map(e => [d, e].flat())));
+
+    let tempArr = [];
+
+    let result;
+
+    if (numberOfSyllabes == 2) {
+      result = cartesian(textArr, textArr)
+    }
+    if (numberOfSyllabes == 3) {
+      result = cartesian(textArr, textArr, textArr)
+    }
+    if (numberOfSyllabes == 4) {
+      result = cartesian(textArr, textArr, textArr, textArr)
+    }
+    if (numberOfSyllabes == 5) {
+      result = cartesian(textArr, textArr, textArr, textArr, textArr)
+    }
+
+    result = Array.from(new Set(result));
+
+    let listItems = result.map((item) => <span> {item}</span>)
 
     setResultText(listItems);
   }
@@ -25,11 +51,14 @@ function App() {
             <p className='info'>This tool will help you create fantasy names. </p>
             <p className='info'>Just type few syllabes in the left column, separating by the commas, and choose the length of the words and get the results in the right column.</p>
             <div className='number-of-syllabes'>
-              <label for="numberOfSyllabes">number of syllabes:  </label>
+              <label htmlFor="numberOfSyllabes">number of syllabes:  </label>
               <select name="numberOfSyllabes" id="numberOfSyllabes" onChange={(event) => {
                 setNumberOfSyllabes(event.target.value);
-              }}>
-                <option value="3" selected="selected">3</option>
+              }}
+                defaultValue={2}
+              >
+                <option value="2">2</option>
+                <option value="3">3</option>
                 <option value="4">4</option>
                 <option value="5">5</option>
               </select>
@@ -44,7 +73,7 @@ function App() {
             </textarea>
           </div>
           <div className='right-column'>
-            <ul>{resultText}</ul>
+            {resultText}
           </div>
         </div>
       </div>
